5d0c921253e0c951b275dbe40ef8f88c95686e85
damodhar-meshram+1-at-595987277759 <damodhar.meshram@kitaboo.com>
2023-10-01 17:00:42 +0530

    revert code

--- index.js
+++ index.js
@@ -1,19 +1,71 @@
-const express = require('express')
+const express = require("express");
+const {headlines, topic} = require("gnews");
+const app = express();
+const port = 3000;
+const googleTrends = require('google-trends-api');
+const json = require('json-loader');
+const date = require('date-and-time')
+const cors = require('cors');
+const bodyParser = require("body-parser");
+app.use(cors({
+     origin: '*'
+}));
 
-const app = express()
-const PORT = 4000
 
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+
+app.get('/headlines', (req, res) => {
+const now  =  new Date();
+  googleTrends.dailyTrends({
+    geo: 'IN',
+    trendDate: date.format(now,'YYYY-MM-DD'),
+}, function(err, results) {
+    if (err) {
+       console.log(err);
+    } else {
+      res.send(JSON.parse(results).default.trendingSearchesDays)
+      console.log(JSON.parse(results).default.trendingSearchesDays[0].trendingSearches);
+    }
+
+});
+
+  // headlines({n : req.query.n,country:"in"}).then(data =>{
+  //   res.send(data)
+  // })
 })
 
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
+
+app.get('/topic', (req, res) => {
+  topic(req.query.topic,{n : req.query.n,country:"in"}).then(data =>{
+    res.send(data)
+  })
 })
 
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
+app.get('/geo', (req, res) => {
+  topic(req.query.geo,{n : req.query.n, country:"in"}).then(data =>{
+    res.send(data)
+  })
 })
 
-// Export the Express API
-module.exports = app
+app.post('/newsfortwitter', (req, res) => {
+  console.dir(req.body);
+
+  data = loader.load()
+  const fprompt = `Write a ${req.body.mode} tweet about ${req.body.content} within 120 characters with hashtags.`
+
+  // completions = OpenAI.co
+
+   res.send({"status" : 200,"tweet" :"your twitter message goes here"})
+})
+
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
+});
